193853f913f2785688a38ba2218caaaaf04820a3
The-Inscrutable-X <68402857+The-Inscrutable-X@users.noreply.github.com>
2022-02-12 10:47:47 -0500

    short commit

--- webscraping.py
+++ webscraping.py
@@ -20,40 +20,43 @@ def text_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     texts = soup.findAll(text=True)
     visible_texts = filter(mask_visible, texts)
-    '''
-    for x, i in enumerate(visible_texts):
-        print(i)
-        if x>5:
-            break
-    '''
+    print('type', type(visible_texts))
     return u" ".join(t.strip() for t in visible_texts)
 
-'''setup selenium'''
+def parse_another_site(response_object, driver, csv_writer):
+    url = next(response_object)
+    driver.get(url)
+    text = text_from_html(driver.page_source).split(' ')
+    print('\n gotten text: ', type(text[:500]), 'Sentence count: ',len(text), '\n')
+
+    pass
+
+"""
+setup selenium
+"""
 #print(os.environ['PATH'])
 os.environ['PATH'] += r';D:/Selenium_webautomation_drivers'
 options = Options()
 options.headless = False
 driver = webdriver.Chrome(options=options)
 
-#googlesearch query
-#breakfast, the first response brings us to a good connection with japan '朝ご飯', https://www.kurashiru.com/lists/d5d8b53c-5cf2-4c4b-b623-9f95ca0666ab
-#the problem is that curated information souces, like geeksforgeeks often lack detailed or up to date information.
+"""
+googlesearch query
+breakfast, the first response brings us to a good connection with japan '朝ご飯', https://www.kurashiru.com/lists/d5d8b53c-5cf2-4c4b-b623-9f95ca0666ab
+the problem is that curated information souces, like geeksforgeeks often lack detailed or up to date information.
+"""
+
 query = '朝ご飯'
 #response = search(query, tld='co.in', num = 10, stop = 10, pause = 2)
 response = search(query, tld='co.in', pause = 2)
-f = open('storage.csv', 'w', encoding='utf8')
-for x,i in enumerate(response):
-    print(x, i)
-    if True:
-        driver.get(i)
-        text = text_from_html(driver.page_source).split(' ')
-        print('\n gotten text: ', type(text[:500]), '\n')
-        #print('\n', 'Source:    ',  driver.page_source[:300],'\n')
-        f.writerows(text)
-    if x > 3:
+with open('storage.csv', 'w', encoding='utf8') as f:
+    csv_writer = csv.writer(f)
+    for x in range(3):
+        print(x)
+        parse_another_site(response, driver, csv_writer)
+    else:
         print('finished')
-        break
-f.close()
+
 
 
 driver.quit()
