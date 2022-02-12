71c589d8015b80200e7cba9b9d8daea940d1af72
The-Inscrutable-X <68402857+The-Inscrutable-X@users.noreply.github.com>
2022-02-12 15:13:50 -0500

    web scraping now returns csv file of sentences, this took so long!

--- webscraping.py
+++ webscraping.py
@@ -21,8 +21,33 @@ def mask_visible(element):
 def text_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     texts = soup.findAll(text=True)
-    visible_texts = filter(mask_visible, texts)
-    #return " ".join(i.strip() for i in visible_texts)
+    output = ''
+    blacklist = [
+        '[document]',
+        'noscript',
+        'header',
+        'html',
+        'meta',
+        'head',
+        'input',
+        'script',
+        # there may be more elements you don't want, such as "style", etc.
+    ]
+    for t in texts:
+        if mask_visible(t):
+            '''if t.parent.name == 'a':
+                output += str(t)
+            elif last_t_type == 'a':
+                for s in re.split('\.', t):
+                    output += str(t)+'|'
+            elif t.parent.name != 'a':
+                for s in re.split('\.', t):
+                    if len(s)>20:
+                        output += str(t)+'|'
+            last_t_type = t.parent.name'''
+            #for s in re.split('\.', t):
+            #    output += str(t)+'|'
+            output += '{}'.format(t)
 
     '''visible_texts = (i.get_text() for i in texts)
     for x,i in enumerate(visible_texts):
@@ -33,14 +58,15 @@ def text_from_html(html):
     def generator(texts):
         for t in texts:
             pass'''
-    return ((s+'.').strip() for t in visible_texts if (t != '\n') for s in re.split('\.', t) if (len(s)>20 and t.parent.name != 'a'))
+    #((s+'.').strip() for t in visible_texts if (t != '\n') for s in re.split('\.', t) if (len(s)>20 and t.parent.name != 'a'))
+    return [i.strip() for i in re.split('\.|\n', output) if len(i)>20]
 
 def parse_another_site(response_object, driver, f):
     url = next(response_object)
     driver.get(url)
     text = text_from_html(driver.page_source)
 
-    #print('\n gotten text: ', url, type(text), 'Sentence count: ',len(text), '\n')
+    print('\n gotten text: ', url, type(text), 'Sentence count: ',len(text), '\n')
     for i in text:
         f.writelines('"'+i+'",\n')
     pass
@@ -58,9 +84,10 @@ driver = webdriver.Chrome(options=options)
 googlesearch query
 breakfast, the first response brings us to a good connection with japan '朝ご飯', https://www.kurashiru.com/lists/d5d8b53c-5cf2-4c4b-b623-9f95ca0666ab
 the problem is that curated information souces, like geeksforgeeks often lack detailed or up to date information.
+english query = lightning
 """
 
-query = 'lightning'
+query = '朝ご飯'
 #response = search(query, tld='co.in', num = 10, stop = 10, pause = 2)
 response = search(query, tld='co.in', pause = 2)
 with open('storage.csv', 'w', encoding='utf8') as f:
