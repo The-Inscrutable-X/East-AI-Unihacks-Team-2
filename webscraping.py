a479e42bda0ac065abf75aa71e3c0d3bd20b21a5
The-Inscrutable-X <68402857+The-Inscrutable-X@users.noreply.github.com>
2022-02-12 12:40:10 -0500

    text parsed from html

--- webscraping.py
+++ webscraping.py
@@ -6,10 +6,12 @@ from bs4.element import Comment
 import requests
 from googlesearch import search
 import csv
+import re
 
 #make more sentences for Audrey
 #tell visible elements from invisible ones manually
 def mask_visible(element):
+    #element.is_displayed()
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
         return False
     if isinstance(element, Comment):
@@ -20,15 +22,21 @@ def text_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     texts = soup.findAll(text=True)
     visible_texts = filter(mask_visible, texts)
-    print('type', type(visible_texts))
-    return u" ".join(t.strip() for t in visible_texts)
+    print('type', type(texts))
+    def generator(texts):
+        for t in texts:
 
-def parse_another_site(response_object, driver, csv_writer):
+        pass
+    return ((s+'.').strip() for t in visible_texts if (t != '\n') for s in re.split('\.', t) if len(s)>20)
+
+def parse_another_site(response_object, driver, f):
     url = next(response_object)
     driver.get(url)
-    text = text_from_html(driver.page_source).split(' ')
-    print('\n gotten text: ', type(text[:500]), 'Sentence count: ',len(text), '\n')
+    text = text_from_html(driver.page_source)
 
+    #print('\n gotten text: ', url, type(text), 'Sentence count: ',len(text), '\n')
+    for i in text:
+        f.writelines('"'+i+'",\n')
     pass
 
 """
@@ -46,17 +54,15 @@ breakfast, the first response brings us to a good connection with japan '朝ご�
 the problem is that curated information souces, like geeksforgeeks often lack detailed or up to date information.
 """
 
-query = '朝ご飯'
+query = 'lightning'
 #response = search(query, tld='co.in', num = 10, stop = 10, pause = 2)
 response = search(query, tld='co.in', pause = 2)
 with open('storage.csv', 'w', encoding='utf8') as f:
-    csv_writer = csv.writer(f)
+    #csv_writer = csv.writer(f)
     for x in range(3):
         print(x)
-        parse_another_site(response, driver, csv_writer)
+        parse_another_site(response, driver, f)
     else:
         print('finished')
-
-
 
 driver.quit()
